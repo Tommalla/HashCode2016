f87691d72d1a8ec2e730b179a8336a699f7f6ad2
michelangelo <michal.modras@gmail.com>
2016-02-11 20:21:26 +0100

    first

--- src/michal.hpp
+++ src/michal.hpp
@@ -1,6 +1,36 @@
 #ifndef __MICHAL_HPP
 #define __MICHAL_HPP
+#include "api.hpp"
 
+struct order {
+	coords destination;
+	productsPerTypeVector products;
+};
 
+int TIME = 0;
+std::vector<order> orders;
+
+inline bool cmp(const order& o1, const order& o2) {
+	return o1.products.size() < o2.products.size();
+}
+
+void initOrders() {
+	for (int i = 0; i < numOfOrders; ++i) {
+		order current;
+		order.destination = orderDestination[i];
+		order.products = numberOfProductsPerOrder[i];
+		orders.push_back(current);
+	}
+}
+
+void calculate() {
+
+	initOrders();	
+	std::sort(orders.begin(), orders.end(), cmp);
+	
+	
+	
+	
+}
 
 #endif
