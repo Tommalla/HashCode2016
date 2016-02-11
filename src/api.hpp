2173d150b4a7a8b925e62670cecfb71054521095
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2016-02-11 19:58:41 +0100

    Wydzielone api.

---
+++ src/api.hpp
@@ -0,0 +1,120 @@
+#ifndef __API_HPP
+#define __API_HPP
+#include <vector>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+
+using namespace std;
+
+// Defs
+
+typedef std::pair<int,int> coords;
+typedef std::vector<int> prodcutsWeightVector;
+typedef std::vector<coords> warehouseCordsVector;
+typedef std::vector<int> productsPerTypeVector;
+
+struct DroneCommand {
+	int droneId;
+	char command;
+	
+	void printCommand() {
+		cout << droneId << " " << command << " " ;
+		printImplem();
+		cout << endl;
+	}
+	
+	virtual void printImplem() = 0;
+	
+	virtual ~DroneCommand() {};
+};
+
+struct DoThingsCommand : public DroneCommand {
+	int special, productType, productQty;
+	
+	void printImplem() override {
+		cout << special << " " << productType << " " << productQty;
+	}
+};
+
+struct WaitCommand : public DroneCommand {
+	int time;
+	
+	void printImplem() override {
+		cout << time;
+	}
+};
+
+DroneCommand* load(const int droneId, const int warehouseId, const int productType, const int productQty) {
+	auto* res = new DoThingsCommand();
+	res->droneId = droneId;
+	res->command = 'L';
+	res->special = warehouseId;
+	res->productType = productType;
+	res->productQty = productQty;
+	return res;
+}
+
+DroneCommand* unload(const int droneId, const int warehouseId, const int productType, const int productQty) {
+	auto* res = load(droneId, warehouseId, productType, productQty);
+	res->command = 'U';
+	return res;
+}
+
+DroneCommand* deliver(const int droneId, const int orderId, const int productType, const int productQty) {
+	auto* res = load(droneId, orderId, productType, productQty);
+	res->command = 'D';
+	return res;
+}
+	
+DroneCommand* wait(const int droneId, const int time) {
+	auto* res = new WaitCommand();
+	res->droneId = droneId;
+	res->command = 'W';
+	res->time = time;
+	return res;
+}
+
+// Func:
+
+// Outputs any errors in solution to stderr along with the result for the solution;
+int validateSolution() {
+	int result = 0;
+	
+	
+	cerr << "Solution value: " << result << endl;
+	return result;
+}
+
+// Containers:
+
+//Matrix:
+int rows;
+int columns;
+
+//Drones:
+int numOfDrones;
+int maxPayload;
+
+//Other:
+int turns;
+
+//Products
+int numOfProducts;
+prodcutsWeightVector productsWeight;
+
+//Warehouses
+int numOfWarehouse;
+warehouseCordsVector WarehouseCoords;
+std::vector<productsPerTypeVector> productsPerWarehousePerType;
+
+//Orders
+int numOfOrders;
+std::vector<coords> orderDestination;
+std::vector<productsPerTypeVector> numberOfProductsPerOrder;
+
+
+vector<DroneCommand *> droneCommands;
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,4 @@
-#include <vector>
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
-#include <set>
-
-using namespace std;
-
-typedef std::pair<int,int> coords;
-typedef std::vector<int> prodcutsWeightVector;
-typedef std::vector<coords> warehouseCordsVector;
-typedef std::vector<int> productsPerTypeVector;
-
-struct DroneCommand {
-	int droneId;
-	char command;
-	
-	void printCommand() {
-		cout << droneId << " " << command << " " ;
-		printImplem();
-		cout << endl;
-	}
-	
-	virtual void printImplem() = 0;
-	
-	virtual ~DroneCommand() {};
-};
-
-struct DoThingsCommand : public DroneCommand {
-	int special, productType, productQty;
-	
-	void printImplem() override {
-		cout << special << " " << productType << " " << productQty;
-	}
-};
-
-struct WaitCommand : public DroneCommand {
-	int time;
-	
-	void printImplem() override {
-		cout << time;
-	}
-};
-
-DroneCommand* load(const int droneId, const int warehouseId, const int productType, const int productQty) {
-	auto* res = new DoThingsCommand();
-	res->droneId = droneId;
-	res->command = 'L';
-	res->special = warehouseId;
-	res->productType = productType;
-	res->productQty = productQty;
-	return res;
-}
-
-DroneCommand* unload(const int droneId, const int warehouseId, const int productType, const int productQty) {
-	auto* res = load(droneId, warehouseId, productType, productQty);
-	res->command = 'U';
-	return res;
-}
-
-DroneCommand* deliver(const int droneId, const int orderId, const int productType, const int productQty) {
-	auto* res = load(droneId, orderId, productType, productQty);
-	res->command = 'D';
-	return res;
-}
-	
-DroneCommand* wait(const int droneId, const int time) {
-	auto* res = new WaitCommand();
-	res->droneId = droneId;
-	res->command = 'W';
-	res->time = time;
-	return res;
-}
-
-//Matrix:
-int rows;
-int columns;
-
-//Drones:
-int numOfDrones;
-int maxPayload;
-
-//Other:
-int turns;
-
-//Products
-int numOfProducts;
-prodcutsWeightVector productsWeight;
-
-//Warehouses
-int numOfWarehouse;
-warehouseCordsVector WarehouseCoords;
-std::vector<productsPerTypeVector> productsPerWarehousePerType;
-
-//Orders
-int numOfOrders;
-std::vector<coords> orderDestination;
-std::vector<productsPerTypeVector> numberOfProductsPerOrder;
-
-
-vector<DroneCommand *> droneCommands;
-
+#include "api.hpp"
 
 int main(int argc, char const *argv[])
 {
@@ -117,10 +16,15 @@ int main(int argc, char const *argv[])
 	droneCommands.push_back(wait(1, 2));
 	droneCommands.push_back(deliver(0, 2, 3, 10));
 	
-	
+	int solutionValue = validateSolution();
+	if (solutionValue < 0) {
+		return -1;
+	}
+    
 	for (DroneCommand* command : droneCommands) {
 		command->printCommand();
 		delete command;
 	}
-	return 0;
+	
+	return solutionValue;
 }
