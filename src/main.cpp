baca2ac60adf7804967aabe7c63245abb779970b
Andrzej <andrzejjac@gmail.com>
2016-02-11 18:27:35 +0100

    start

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
-int main() {
-	return 0;
+int main(int argc, char const *argv[])
+{
+    std::vector<char> v;
+    if (FILE *fp = fopen("filename", "r"))
+    {
+        char buf[1024];
+        while (size_t len = fread(buf, 1, sizeof(buf), fp))
+            v.insert(v.end(), buf, buf + len);
+        fclose(fp);
+    }
+    return 0;
 }
