37817f2f039cabc3862bdbd693a2dd75f138363f
Andrzej <andrzejjac@gmail.com>
2016-02-11 19:14:35 +0100

    input element

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,43 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <set>
+
+typedef std::pair<int,int> coords;
+typedef std::vector<int> prodcutsWeightVector;
+typedef std::vector<coords> warehouseCordsVector;
+typedef std::vector<int> productsPerTypeVector;
+
+//Matrix:
+int rows;
+int columns;
+
+//Drones:
+int numOfDrones;
+int maxPayload;
+
+//Other:
+int turns;
+
+//Products
+int numOfProducts;
+prodcutsWeightVector productsWeight;
+
+//Warehouses
+int numOfWarehouse;
+warehouseCordsVector WarehouseCoords;
+std::vector<productsPerTypeVector> productsPerWarehousePerType;
+
+//Orders
+int numOfOrders;
+std::vector<coords> orderDestination;
+std::vector<productsPerTypeVector> numberOfProductsPerOrder;
+
+
+
 
 int main(int argc, char const *argv[])
 {
-    std::vector<char> v;
  	std::ifstream file("input");
  	
  	int line_num = 0;
